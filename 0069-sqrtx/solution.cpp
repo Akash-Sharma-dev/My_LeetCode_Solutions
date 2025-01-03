b973fb39bd59e0ae1af7e0c0e253bbeb60c5f97f
Akash Sharma <akashsharmaa7492@gmail.com>
2025-01-03 13:53:49 +0000

    Sync LeetCode solution Runtime - 0 ms (100.00%), Memory - 8.5 MB (84.84%)

---
+++ 0069-sqrtx/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0) return 0;
+            int left = 1,mid=1,right = x/2;
+                while(left<=right){
+                        mid = left + (right-left)/2;
+                                if(mid<=x/mid){
+                                           if(mid+1>x/(mid+1)){
+                                                          return mid;
+                                                                     }
+                                                                                left=mid+1;
+                                                                                        }
+                                                                                                else{
+                                                                                                           right = mid-1;
+                                                                                                                   }
+                                                                                                                       }
+                                                                                                                           return mid;
+    }
+};
