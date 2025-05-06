d27d337d74387d58fe6b35b3ff46924dbab9fcaa
Akash Sharma <akashsharmaa7492@gmail.com>
2025-05-06 16:09:24 +0000

    Sync LeetCode solution Runtime - 4 ms (16.68%), Memory - 9.9 MB (53.92%)

--- 0125-valid-palindrome/solution.cpp
+++ 0125-valid-palindrome/solution.cpp
@@ -1,25 +1,16 @@
 class Solution {
-    public:
-        bool isPalindrome(string s) {
-                int left = 0;
-                        int right = s.length() - 1;
-
-                                while (left < right) {
-                                            // Move left pointer to the next alphanumeric character
-                                                        while (left < right && !isalnum(s[left])) {
-                                                                        left++;
-                                                                                    }
-                                                                                                // Move right pointer to the previous alphanumeric character
-                                                                                                            while (left < right && !isalnum(s[right])) {
-                                                                                                                            right--;
-                                                                                                                                        }
-                                                                                                                                                    // Compare the characters (case insensitive)
-                                                                                                                                                                if (tolower(s[left]) != tolower(s[right])) {
-                                                                                                                                                                                return false;
-                                                                                                                                                                                            }
-                                                                                                                                                                                                        left++;
-                                                                                                                                                                                                                    right--;
-                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                    return true;
-                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                        };
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right){
+        while(left<right && !isalnum(s[left])) left++;
+        while(left<right && !isalnum(s[right])) right--;
+        if(tolower(s[left]) != tolower(s[right])) return false;
+        left++;
+        right--;
+        }
+        return true;
+    }
+};
+    
