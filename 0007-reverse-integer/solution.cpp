c3f2d370a3ec0d9042ee522225921de6df062e8c
Akash Sharma <akashsharmaa7492@gmail.com>
2025-05-22 11:33:01 +0000

    Sync LeetCode solution Runtime - 5 ms (19.88%), Memory - 8.6 MB (53.79%)

--- 0007-reverse-integer/solution.cpp
+++ 0007-reverse-integer/solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int reverse(int x) {
-        int digit;
-            int reverse = 0;
-                while(x){
-                        digit = x%10;
-                                if(reverse>INT_MAX/10 || reverse<INT_MIN/10) return 0;
-                                        reverse = reverse*10 + digit;
-                                                x/=10;
-                                                    }
-                                                        return reverse;
+        int reverse = 0;
+        while(x){
+        if((reverse>INT_MAX/10 || (reverse==INT_MAX/10 && x%10>INT_MAX%10)) || (reverse<INT_MIN/10 || (reverse==INT_MIN/10 && x%10<INT_MIN%10))) return 0;
+        reverse = reverse*10+x%10;
+
+        x /=10;
+
+    }
+
+    return reverse;
     }
 };
