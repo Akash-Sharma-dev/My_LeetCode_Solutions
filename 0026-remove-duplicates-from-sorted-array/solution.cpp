f319c58fe9047e5afd275ec0deb6fb570cca791d
Akash Sharma <akashsharmaa7492@gmail.com>
2025-05-25 11:00:28 +0000

    Sync LeetCode solution Runtime - 0 ms (100.00%), Memory - 22.6 MB (51.61%)

--- 0026-remove-duplicates-from-sorted-array/solution.cpp
+++ 0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_map<int,int> digit;
-        vector<int> unique;
-        int n = nums.size();
-        int x = 0;
-        for(int i = 0;i<n;i++){
-            digit[nums[i]] += 1;
-            if(digit[nums[i]] == 1){                nums[x] = nums[i];
-               x++;
+        int x = 0,n = nums.size();
+        for(int i = 1;i<n;i++){
+            if(nums[x] < nums[i]){
+                x++;
+                nums[x] = nums[i];
             }
-            
         }
-        return x;
+        return x+1;
     }
 };
