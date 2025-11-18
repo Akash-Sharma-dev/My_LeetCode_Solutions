9c9004cb027afffc6b3396d7b73027968f9df9f6
Akash Sharma <akashsharmaa7492@gmail.com>
2025-11-18 12:57:37 +0000

    Sync LeetCode solution Runtime - 0 ms (100.00%), Memory - 9.4 MB (89.97%)

---
+++ 0225-implement-stack-using-queues/solution.cpp
@@ -0,0 +1,44 @@
+class MyStack {
+    queue<int> q1;
+    int total = 0;
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q1.push(x);
+        total++;
+        int count = total;
+        while(count != 1){
+            q1.push(q1.front());
+            q1.pop();
+            count--;
+        }
+        cout<<endl;
+    }
+    
+    int pop() {
+        total--;
+        int del = q1.front();
+        q1.pop();
+        return del;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
