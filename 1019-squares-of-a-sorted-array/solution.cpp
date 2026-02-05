991ba5356ce2417032538de394251a3979d4278e
Akash Sharma <akashsharmaa7492@gmail.com>
2026-02-05 04:46:57 +0000

    Sync LeetCode solution Runtime - 0 ms (100.00%), Memory - 30.3 MB (65.15%)

--- 1019-squares-of-a-sorted-array/solution.cpp
+++ 1019-squares-of-a-sorted-array/solution.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i = 0;i<nums.size();i++){
-            nums[i] *= nums[i];
-        }
-        sort(nums.begin(), nums.end());
-        return nums;
+        int n = nums.size();
+        int l = 0, r = n-1, j = n-1,sq_l, sq_r;
+        vector<int>arr(n);
 
+        while(l<=r){
+             sq_l = nums[l] * nums[l];
+             sq_r = nums[r] * nums[r];
+
+             if(sq_l > sq_r){
+                arr[j] = sq_l;
+                l++;
+             }else{
+                arr[j] = sq_r;
+                r--;
+             }
+             j--;
+        }
+        return arr;
     }
 };
