bdb27b1dc27bd3369be4b9366b9f0273b5490a52
Akash Sharma <akashsharmaa7492@gmail.com>
2026-02-07 12:34:29 +0000

    Sync LeetCode solution Runtime - 0 ms (100.00%), Memory - 29.5 MB (68.05%)

--- 0189-rotate-array/solution.cpp
+++ 0189-rotate-array/solution.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k = k%nums.size();
-        if(k==0) return;
+        k%=nums.size();
         reverse(nums.begin(),nums.end()-k);
         reverse(nums.end()-k,nums.end());
         reverse(nums.begin(),nums.end());
