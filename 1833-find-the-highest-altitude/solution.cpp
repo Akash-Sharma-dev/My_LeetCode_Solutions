21945ae58d25b72b3c3b80d73a084c768733cdf9
Akash Sharma <akashsharmaa7492@gmail.com>
2026-02-07 16:10:51 +0000

    Sync LeetCode solution Runtime - 0 ms (100.00%), Memory - 10.9 MB (45.13%)

--- 1833-find-the-highest-altitude/solution.cpp
+++ 1833-find-the-highest-altitude/solution.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int sum = 0, alt = 0, n = gain.size();
+        int alt = 0, h = 0, n = gain.size();
         for(int i = 0; i<n; i++){
-            sum += gain[i];
-            alt = max(alt, sum);
+            h += gain[i];
+            if(h>alt) alt = h;
         }
         return alt;
     }
