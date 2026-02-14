fda343c7cfffc8c91d57e3cbe670463e014b7282
Akash Sharma <akashsharmaa7492@gmail.com>
2026-02-14 18:31:59 +0000

    Code challenge submission for question: 80 Remove Duplicates from Sorted Array II

---
+++ 80 Remove Duplicates from Sorted Array II/80remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size(),count = 1,k = 1;
+        for(int i = 1; i<n; i++){
+            if(nums[i] != nums[i-1]) count = 1;
+            else count++;
+            if(count > 2) continue;
+            nums[k++] = nums[i];
+        }
+        return k;
+    }
+};
