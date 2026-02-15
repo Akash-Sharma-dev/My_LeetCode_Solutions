2b4e9efc8120df3d50a6ae09690aa5d77c0d7b70
Akash Sharma <akashsharmaa7492@gmail.com>
2026-02-15 06:11:00 +0000

    Sync LeetCode solution Runtime - 0 ms (100.00%), Memory - 26 MB (78.82%)

--- 0042-trapping-rain-water/solution.cpp
+++ 0042-trapping-rain-water/solution.cpp
@@ -1,33 +1,33 @@
 class Solution {
-    public:
-        int trap(vector<int>& height) {
-            int water = 0;
-            int n = height.size();
-            int current = 0;
-            int last = 1;
-            int large = last;
-            while(current+1<n){
-                 last = current+1;
-                 large = last;
-                 while(last<n && height[last]<height[current]){
-                     if(height[large]<=height[last]){
-                                                           large = last;
-                                                                    }
+public:
+    int trap(vector<int>& height) {
+        int n = height.size(),maxi = 0, max_i = n-1,water = 0, total = 0,left = 0, right = n-1;
 
-                                                         last++;
-                  }
-                                                  if(last<n){
-                                                    for(int i = current+1;i<last;i++){
-                    water += height[current]-height[i];
-                                                  }
-                                                    current = last;
-             }else{
-               for(int i = current+1;i<large;i++){
-                    water += height[large]-height[i];
-             }
-               current = large;
-             }
-                                             }
-        return water;  
+        for(int i = 0; i<n; i++){
+            if(height[i] > maxi){
+                maxi = height[i];
+                max_i = i;
+            }
+        }
+        while(left < max_i){
+            int j = left+1;
+            while(height[j] < height[left]){
+                water  = height[left] - height[j];
+                total += water;
+                j++;
+            }
+            left = j;
+        }
+        while(right > max_i){
+            int j = right-1;
+            while(height[j] < height[right]){
+                water  = height[right] - height[j];
+                total += water;
+                j--;
+            }
+            right = j;
+        }
+        
+        return total;
     }
 };
