9bcf97b6453088bfa20e85a02e67b750e632fcd0
Akash Sharma <akashsharmaa7492@gmail.com>
2026-02-18 03:58:06 +0000

    Sync LeetCode solution Runtime - 3 ms (98.11%), Memory - 11.3 MB (54.74%)

--- 0142-linked-list-cycle-ii/solution.cpp
+++ 0142-linked-list-cycle-ii/solution.cpp
@@ -9,23 +9,23 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-       ListNode *slow = head;
-           ListNode *fast = head;
-               while(fast!=NULL && fast->next!=NULL){
-                       slow=slow->next;
-                               fast=fast->next->next;
-                                       if(slow==fast){
-                                                   break;
-                                                           }
-                                                               }
-                                                                   if(fast==NULL || fast->next==NULL){
-                                                                           return NULL;
-                                                                               }
-                                                                                   slow = head;
-                                                                                       while(slow!=fast){
-                                                                                               slow=slow->next;
-                                                                                                       fast=fast->next;
-                                                                                                           }
-                                                                                                               return slow; 
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast!=NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast){
+                break;
+            }
+        }
+        slow = head;
+        while(fast!=NULL && fast->next != NULL){
+            if(slow==fast){
+                return slow;
+            }
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return NULL;
     }
 };
