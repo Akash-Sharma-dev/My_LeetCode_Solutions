2284d53e282afcd9f9a2da9cb317a4b087f3c87c
Akash Sharma <akashsharmaa7492@gmail.com>
2026-02-25 06:26:25 +0000

    Code challenge submission for question: 61 Rotate List

---
+++ 61 Rotate List/61rotate-list.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        ListNode *temp = head;
+        int count = 1;
+        if(head==NULL) return head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+            count++;
+        }
+        k = k%count;
+        int start = count - k;
+        count = 1;
+        ListNode *cur = head;
+        while(count<start){
+            cur = cur->next;
+            count++;
+        }
+        temp->next = head;
+        head = cur->next;
+        cur->next =NULL;
+        return head;
+
+    }
+};
