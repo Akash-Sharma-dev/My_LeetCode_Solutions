d679248c57df01bfe5b5d080efa27fa1ec06f07a
Akash Sharma <akashsharmaa7492@gmail.com>
2026-02-27 12:22:44 +0000

    Code challenge submission for question: 350 Intersection of Two Arrays II

---
+++ 350 Intersection of Two Arrays II/350intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        int n1 = nums1.size(), n2 = nums2.size();
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int p1 = 0, p2 = 0;
+        while(p1<n1 && p2<n2){
+            if(nums1[p1] == nums2[p2]){
+                ans.push_back(nums1[p1]);
+                p1++;
+                p2++;
+            }else if(nums1[p1] < nums2[p2]){
+                p1++;
+            }else{
+                p2++;
+            }
+        }
+        return ans;
+    }
+};
