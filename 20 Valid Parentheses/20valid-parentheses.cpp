6626e9bc874730713585027b003f3cb9f2228fd1
Akash Sharma <akashsharmaa7492@gmail.com>
2026-04-06 09:12:54 +0000

    Code challenge submission for question: 20 Valid Parentheses

---
+++ 20 Valid Parentheses/20valid-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isValid(string s) {
+     stack<char>st;
+     int n = s.length();
+     for(int i = 0; i<n; i++){
+        if(!st.empty() && ((s[i] == ')' && st.top() == '(') || (s[i] == ']' && st.top() == '[') || (s[i] == '}' && st.top() == '{'))){
+            st.pop();
+        }else{
+            st.push(s[i]);
+        }
+     }
+     return st.empty();
+    }
+};
