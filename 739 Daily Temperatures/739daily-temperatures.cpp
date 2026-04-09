67504e0b2030278f73907dc356bec83faf834971
Akash Sharma <akashsharmaa7492@gmail.com>
2026-04-09 05:09:57 +0000

    Code challenge submission for question: 739 Daily Temperatures

---
+++ 739 Daily Temperatures/739daily-temperatures.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int>st;
+        int n = temperatures.size();
+        vector<int>res(n);
+        for(int i = 0; i<n; i++){
+            while(!st.empty() && temperatures[st.top()] < temperatures[i]){
+               res[st.top()] = i - st.top();
+               st.pop(); 
+            }
+            st.push(i);
+        }
+        while(!st.empty()){
+            res[st.top()] = 0;
+            st.pop();
+        }
+        return res;
+    }
+};
