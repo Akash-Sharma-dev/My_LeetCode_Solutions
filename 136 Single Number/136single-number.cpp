0ad0dc049a72f479f937c24b83e99e0d6604b554
Akash Sharma <akashsharmaa7492@gmail.com>
2026-04-22 12:50:54 +0000

    Code challenge submission for question: 136 Single Number

---
+++ 136 Single Number/136single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size(), xr = 0;
+        for(int i = 0; i<n; i++){
+            xr ^= nums[i];
+        }
+        return xr;
+    }
+};
