0afee8b6226de6819ff3ad04b215171d974b0f4a
Akash Sharma <akashsharmaa7492@gmail.com>
2026-04-24 06:57:35 +0000

    Code challenge submission for question: 73 Set Matrix Zeroes

---
+++ 73 Set Matrix Zeroes/73set-matrix-zeroes.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size(), row1 = 0, col1 = 0;
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    if(i==0) row1 = 1;
+                    if(j==0) col1 = 1;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<m; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(row1){
+            for(int j = 0; j<m; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if(col1){
+            for(int i = 0; i<n; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
