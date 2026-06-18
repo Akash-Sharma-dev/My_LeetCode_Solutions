e746db610ed0404e3d1458f1a01874fa18fdadfc
Akash Sharma <akashsharmaa7492@gmail.com>
2026-06-18 06:14:11 +0000

    Code challenge submission for question: 112 Path Sum

---
+++ 112 Path Sum/112path-sum.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root)
+            return false;
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
+        while(!q.empty()){
+            TreeNode *cur = q.front().first;
+            int csum =  q.front().second;
+            q.pop();
+            if(!cur->left && !cur->right){
+                if(csum+cur->val == targetSum){
+                    return true;
+                }
+            }
+            if(cur->left)
+            q.push({cur->left,csum+cur->val});
+            if(cur->right)
+            q.push({cur->right,csum+cur->val});
+        }
+        return false;
+    }
+};
